574602ed5174e213fdd8f70cb8bf400fb8091f93
César Magalhães <ceanma83@gmail.com>
2021-11-04 18:54:24 -0300

    Change main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte("<h1>This ArgoCD test</h1>"))
+		rw.Write([]byte("<h1>This ArgoCD App</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
